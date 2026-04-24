4236ae56c5c4df201fa8bd31f2ff97896b0be064
Hans Correa <hans.correa@venturatravel.org>
2026-04-24 13:04:14 -0500

    Change date

--- src/constants/landing.ts
+++ src/constants/landing.ts
@@ -1,6 +1,6 @@
 // April 22 2026 at midnight Colombia time (UTC-5).
 // Change this date to reschedule the launch countdown everywhere it appears.
-export const LAUNCH_DATE = new Date("2026-04-24T00:00:00-05:00")
+export const LAUNCH_DATE = new Date("2026-04-29T00:00:00-05:00")
 
 export const SECTIONS_IDS = {
     allies: "allies",
