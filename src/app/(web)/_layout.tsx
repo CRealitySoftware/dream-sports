922b42dbfdf2a9889cca1e14dc62763bf5092e6f
Hans Correa <hans.correa@venturatravel.org>
2026-05-06 21:11:29 -0500

    Added title

--- src/app/(web)/_layout.tsx
+++ src/app/(web)/_layout.tsx
@@ -1,16 +1,22 @@
 import WhatsAppButton from "@/components/ui/WhatsAppButton";
 import Navbar from "@/components/website/landing/Navbar";
 import { Stack } from "expo-router";
+import Head from "expo-router/head";
 import { View } from "react-native";
 
 export default function WebLayout() {
   return (
-    <View style={{ flex: 1 }}>
-      <Navbar />
-      <View style={{ paddingTop: 64, flex: 1 }}>
-        <Stack screenOptions={{ headerShown: false }} />
+    <>
+      <Head>
+        <title>Dream Sports</title>
+      </Head>
+      <View style={{ flex: 1 }}>
+        <Navbar />
+        <View style={{ paddingTop: 64, flex: 1 }}>
+          <Stack screenOptions={{ headerShown: false }} />
+        </View>
+        <WhatsAppButton />
       </View>
-      <WhatsAppButton />
-    </View>
+    </>
   );
 }
